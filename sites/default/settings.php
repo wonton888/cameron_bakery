11e344c69425e79c66e73c2f6b7f72b03b50a754
Adam <wonton916@gmail.com>
2015-11-03 15:10:43 -0800

    fix module page for cameron_bakery

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -258,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'qYFo-OXl4X582g_Yw7UNEaMYnrvtSLrKXlMZ-HLYRyI';
+$drupal_hash_salt = 'uweTlnd83CreBgcFksl6F7V7E9YJ4PAORmJEVD15CTg';
 
 /**
  * Base URL (optional).
